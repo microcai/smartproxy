6355354dec90c44cbbbbbc6cee180e0a6167a65d
microcai <microcaicai@gmail.com>
2019-08-19 18:18:12 +0800

    fix multiread_first_pkg

--- src/multireadfirstpkg.hpp
+++ src/multireadfirstpkg.hpp
@@ -10,9 +10,9 @@
 
 struct multiread_first_tag
 {
-	std::atomic_flag first_flag;
+	std::atomic_flag first_flag = ATOMIC_FLAG_INIT;
 
-	bool readed = false;
+	std::atomic_bool readed = false;
 
 	std::array<char, 4096> buf;
 
@@ -35,11 +35,11 @@ namespace detail {
 	template<class Stream, typename MutableBuffers, typename Handler>
 	struct multiread_first_pkg_op : boost::asio::coroutine
 	{
-		multiread_first_pkg_op(multiread_first_tag& tag, Stream& s, const MutableBuffers& buffers, Handler handler)
+		multiread_first_pkg_op(multiread_first_tag& tag, Stream& s, MutableBuffers buffers, Handler&& handler)
 			: tag(tag)
 			, s(s)
-			, buffers(buffers)
-			, handler(handler)
+			, buffer(buffers)
+			, handler((Handler&&)handler)
 		{
 
 		}
@@ -47,7 +47,7 @@ namespace detail {
 		Handler handler;
 		multiread_first_tag& tag;
 		Stream& s;
-		const MutableBuffers& buffers;
+		MutableBuffers buffer;
 
 		typedef void result_type;
 
@@ -58,51 +58,44 @@ namespace detail {
 
 				if (!tag.first_flag.test_and_set())
 				{
-					BOOST_ASIO_CORO_YIELD s.async_read_some(boost::asio::buffer(tag.buf), *this);
+					BOOST_ASIO_CORO_YIELD s.async_read_some(boost::asio::buffer(tag.buf, 4096), *this);
 
-					boost::asio::buffer_copy(buffers, boost::asio::buffer(tag.buf, bytes_transfered));
-
-					handler(ec, bytes_transfered);
+					boost::asio::buffer_copy(buffer, boost::asio::buffer(tag.buf, bytes_transfered));
+					boost::asio::post(s.get_executor(), std::bind(handler, ec, bytes_transfered));
 
 					{
 
 						std::lock_guard<std::mutex> l(tag.lock);
+						tag.readed = true;
+						tag.ec = ec;
+						tag.bytes_transfered = bytes_transfered;
 
 						for (auto h : tag.queued_handlers)
 						{
 							boost::asio::buffer_copy(h.buffer, boost::asio::buffer(tag.buf, bytes_transfered));
-
-							boost::asio::post(s.get_executor(), [ec, bytes_transfered, h]()
-							{
-								h.handler(ec, bytes_transfered);
-							});
+							boost::asio::post(s.get_executor(), std::bind(h.handler, ec, bytes_transfered));
 						}
-
-						tag.readed = true;
-						tag.ec = ec;
-						tag.bytes_transfered = bytes_transfered;
 
 					}
 				}
 				else
 				{
-					// 挂入列队.
+					std::lock_guard<std::mutex> l(tag.lock);
+					if (tag.readed)
 					{
-						std::lock_guard<std::mutex> l(tag.lock);
-						if (tag.readed)
-						{
-							// 过了, 所以直接返回.
-							boost::asio::buffer_copy(buffers,  boost::asio::buffer(tag.buf, tag.bytes_transfered));
-							handler(tag.ec, tag.bytes_transfered);
-						}
-						else
-						{
-							tag.queued_handlers.push_back(multiread_first_tag::queued_reader{
-								handler,
-								buffers
-							});
-						}
+						// 过了, 所以直接返回.
+						boost::asio::buffer_copy(buffer,  boost::asio::buffer(tag.buf, tag.bytes_transfered));
+						boost::asio::post(s.get_executor(), std::bind(handler, tag.ec, tag.bytes_transfered));
 					}
+					else
+					{
+						// 挂入列队.
+						tag.queued_handlers.push_back(multiread_first_tag::queued_reader{
+							handler,
+							buffer
+						});
+					}
+
 				}
 			}
 		}
@@ -113,12 +106,12 @@ namespace detail {
 
 template<class Stream, typename MutableBuffers, typename Handler>
 	BOOST_ASIO_INITFN_RESULT_TYPE(Handler, void(boost::system::error_code, std::size_t))
-multiread_first_pkg(multiread_first_tag& tag, Stream& s, const MutableBuffers& buffers, Handler&& handler)
+multiread_first_pkg(multiread_first_tag& tag, Stream& s, MutableBuffers buffers, Handler&& handler)
 {
 	// ASYNC_HANDLER_TYPE_CHECK(Handler, void(boost::system::error_code, std::size_t));
 	boost::asio::async_completion<Handler, void(boost::system::error_code, std::size_t)> init(handler);
 
-	detail::multiread_first_pkg_op<Stream, MutableBuffers, decltype(init.completion_handler)>(tag, s, buffers, init.completion_handler)();
+	detail::multiread_first_pkg_op<Stream, MutableBuffers, std::decay_t<decltype(init.completion_handler)>>(tag, s, buffers, std::move(init.completion_handler))();
 
 	return init.result.get();
 }
