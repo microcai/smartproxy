4ab51d09c3bfe10a3b64cfd3ef284918b15e9925
microcai <microcaicai@gmail.com>
2019-08-26 10:52:34 +0800

    also fix linux

--- src/ui/cli.cpp
+++ src/ui/cli.cpp
@@ -1,7 +1,26 @@
 
+#include <iostream>
+#include <sys/resource.h>
+
+void ulimit_limit()
+{
+    struct rlimit rlp;
+
+    getrlimit(RLIMIT_NOFILE, &rlp);
+
+    if (rlp.rlim_cur < 10000)
+    {
+        rlp.rlim_cur = 10000;
+        setrlimit(RLIMIT_NOFILE, &rlp);
+    }
+    getrlimit(RLIMIT_NOFILE, &rlp);
+    std::cout << "rlimit changed to " << rlp.rlim_cur << std::endl;
+}
+
 extern int proxy_main(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
+	ulimit_limit();
 	return proxy_main(argc, argv);
 }
