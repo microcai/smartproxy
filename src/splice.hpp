c85a9ea4c7433fd07a568c148261609fbdff9175
microcai <microcaicai@gmail.com>
2019-09-06 17:55:59 +0800

    avoid mem alloc

--- src/splice.hpp
+++ src/splice.hpp
@@ -17,7 +17,7 @@ namespace avsocks{
 
 #define ASIO_WRITE_PLACEHOLDERS asio::placeholders::error,asio::placeholders::bytes_transferred
 
-#define SPLICE_SIZE 1024*1024*64
+#define SPLICE_SIZE 1024*1024
 
 template <class S1, class S2>
 class splice : public boost::enable_shared_from_this<splice<S1,S2> >{
@@ -29,10 +29,10 @@ public:
 		: s1(std::move(_s1)),s2(std::move(_s2)) {}
 
 	void start(){
-		s1.async_read_some(s1s2buf.prepare(SPLICE_SIZE),
+		s1.async_read_some(boost::asio::buffer(s1s2buf),
 			boost::bind(&splice<S1,S2>::s1s2_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 		);
-		s2.async_read_some(s2s1buf.prepare(SPLICE_SIZE),
+		s2.async_read_some(boost::asio::buffer(s2s1buf),
 			boost::bind(&splice<S1,S2>::s2s1_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 		);
 	}
@@ -44,8 +44,7 @@ public:
 private:
 	void s1s2_handle_read(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s1s2buf.commit(bytes_transferred);
-			boost::asio::async_write(s2, s1s2buf, boost::asio::transfer_all(),
+			boost::asio::async_write(s2, boost::asio::buffer(s1s2buf), boost::asio::transfer_exactly(bytes_transferred),
 				boost::bind(&splice<S1,S2>::s1s2_handle_write,shared_from_this(),ASIO_WRITE_PLACEHOLDERS)
 			);
 		}
@@ -62,8 +61,7 @@ private:
 	}
 	void s1s2_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s1s2buf.consume(bytes_transferred);
-			s1.async_read_some(s1s2buf.prepare(SPLICE_SIZE),
+			s1.async_read_some(boost::asio::buffer(s1s2buf),
 				boost::bind(&splice<S1,S2>::s1s2_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 			);
 		}
@@ -75,8 +73,7 @@ private:
 	}
 	void s2s1_handle_read(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s2s1buf.commit(bytes_transferred);
-			boost::asio::async_write(s1, s2s1buf, boost::asio::transfer_all(),
+			boost::asio::async_write(s1, boost::asio::buffer(s2s1buf), boost::asio::transfer_exactly(bytes_transferred),
 				boost::bind(&splice<S1,S2>::s2s1_handle_write,shared_from_this(),ASIO_WRITE_PLACEHOLDERS)
 			);
 		}else if (ec == boost::asio::error::eof){
@@ -91,8 +88,7 @@ private:
 	}
 	void s2s1_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s2s1buf.consume(bytes_transferred);
-			s2.async_read_some(s2s1buf.prepare(SPLICE_SIZE),
+			s2.async_read_some(boost::asio::buffer(s2s1buf),
 				boost::bind(&splice<S1,S2>::s2s1_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 			);
 		}else{
@@ -101,9 +97,9 @@ private:
 		}
 	}
 private:
-	asio::streambuf	s1s2buf,s2s1buf;
-	S1						s1; //两个 socket
-	S2						s2; //两个 socket
+	std::array<char, SPLICE_SIZE>	s1s2buf,s2s1buf;
+	S1								s1; //两个 socket
+	S2								s2; //两个 socket
 };
 
 #undef  ASIO_READ_PLACEHOLDERS
